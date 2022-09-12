26a19087fc535b0561820ef8cf565890dc7063e9
‘Nice233666 <791371161@qq.com>
2022-09-12 20:03:41 +0800

    9.12 首页显示分类菜单

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -3,44 +3,26 @@
     <a-layout-sider width="200" style="background: #fff">
       <a-menu
           mode="inline"
-
           :style="{ height: '100%', borderRight: 0 }"
+          @click="handleClick"
       >
-        <a-sub-menu key="sub1">
-          <template #title>
+        <a-menu-item key="welcome">
+          <router-link :to="'/'">
+            <MailOutlined />
+            <span>欢迎</span>
+          </router-link>
+        </a-menu-item>
+        <a-sub-menu v-for="item in level1" :key="item.id">
+          <template v-slot:title>
               <span>
-                <user-outlined />
-                subnav 1
+                <user-outlined />{{item.name}}
               </span>
           </template>
-          <a-menu-item key="1">option1</a-menu-item>
-          <a-menu-item key="2">option2</a-menu-item>
-          <a-menu-item key="3">option3</a-menu-item>
-          <a-menu-item key="4">option4</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub2">
-          <template #title>
-              <span>
-                <laptop-outlined />
-                subnav 2
-              </span>
-          </template>
-          <a-menu-item key="5">option5</a-menu-item>
-          <a-menu-item key="6">option6</a-menu-item>
-          <a-menu-item key="7">option7</a-menu-item>
-          <a-menu-item key="8">option8</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub3">
-          <template #title>
-              <span>
-                <notification-outlined />
-                subnav 3
-              </span>
-          </template>
-          <a-menu-item key="9">option9</a-menu-item>
-          <a-menu-item key="10">option10</a-menu-item>
-          <a-menu-item key="11">option11</a-menu-item>
-          <a-menu-item key="12">option12</a-menu-item>
+          <a-menu-item v-for="child in item.children" :key="child.id">
+            <span>
+              <MailOutlined />{{child.name}}
+            </span>
+          </a-menu-item>
         </a-sub-menu>
       </a-menu>
     </a-layout-sider>
@@ -73,6 +55,8 @@
 import { defineComponent ,onMounted,ref} from 'vue';
 import axios from "axios";
 import { StarOutlined, LikeOutlined, MessageOutlined } from '@ant-design/icons-vue';
+import {Tool} from "@/utils/tool";
+import {message} from "ant-design-vue";
 
 const listData:any = [];
 
@@ -119,12 +103,35 @@ export default defineComponent({
       { type: 'MessageOutlined', text: '2' },
     ];
 
+    const level1=ref();
+    let categorys: any;
+    /**
+     * 数据查询
+     **/
+    const handleClick = () => {
+      axios.get("/category/all", ).then((response) => {
+        const data = response.data;
+        if (data.success){
+          categorys = data.content;
+          console.log("原始数组:",categorys);
+
+          level1.value=[];
+          level1.value=Tool.array2Tree(categorys,0);
+          console.log("树形数组:",level1.value)
+        }else{
+          message.error(data.message);
+        }
+      });
+    };
+
     return{
       ebooks,
 
       listData,
       pagination,
       actions,
+      level1,
+      handleClick,
     }
   },
 
