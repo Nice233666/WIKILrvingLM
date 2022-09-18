44343ee5b3bf08b8a97f1c208d75ad9d7f9b5e50
‘Nice233666 <791371161@qq.com>
2022-09-18 19:49:17 +0800

    9.18 文档内容表的设计与生成

--- doc/all.sql
+++ doc/all.sql
@@ -19,6 +19,14 @@ create table `demo`(
 
 insert into `demo`(id,name) values(1,'测试');
 
+#文档内容
+drop table if exists `content`;
+create table `content`(
+    `id` bigint not null comment  '文档id',
+    `content` mediumtext not null comment '内容',
+    primary key (`id`)
+)engine= innodb default charset =utf8mb4 comment='文档内容';
+
 #文档表
 drop table if exists `doc`;
 create table `doc`(
